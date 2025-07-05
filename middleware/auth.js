1e3cfc91c41e5b40b6a410d78264ecd432acee14
IndexDDuo <jl.jing02123@gmail.com>
2025-07-05 08:55:22 +0000

    Jul 5, 2025, 4:55 AM

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,8 @@ function checkAuth(req, res, next) {
 	// Check req.session.loggedIn to see if the user is logged in
 	// If the user is logged in, call next()
 	// If the user is not logged in, redirect to /login
+
+	
 }
 
 module.exports = checkAuth
